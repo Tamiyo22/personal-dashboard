1e22049dea754fb1d5a8fc422bec65c2c56484c3
Melissa Aybar <melissaeaybar@gmail.com>
2023-06-17 13:09:37 -0500

    create Sidebar component

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -11,10 +11,11 @@ import {
     UserIcon,
     SunIcon
   } from "@heroicons/react/outline";
+  
 
 export default function Sidebar() {
   return (
-    <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full xl:ml-24">
+    <div className=" hidden sm:flex flex-col p-2 xl:items-start fixed h-full xl:ml-24">
     {/* Twitter Logo */}
     <div className="hoverEffect p-0 hover:bg-blue-100 xl:px-1">
      
@@ -37,18 +38,20 @@ export default function Sidebar() {
         </div>
 
         {/*Button */}
-        <button>Tweet</button>
+        <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">
+            Tweet
+          </button>
 
  {/* Mini profile */}
-      <div className="">
-       
+ <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
+      
         {/* Profile photo goes here */}
-        <h4>
+        <h4 className="font-bold">
             Melissa
         </h4>
         <p>@Melissa</p>
 
-      <DotsHorizontalIcon className="h-5"></DotsHorizontalIcon>
+      <DotsHorizontalIcon className="h-5"/>
         </div>
 
         
--- components/SidebarMenuItem.js
+++ components/SidebarMenuItem.js
@@ -1,10 +1,11 @@
+import { BLOCKED_PAGES } from "next/dist/shared/lib/constants";
 
 
-export default function SidebarMenuItems({text,Icon}) {
+export default function SidebarMenuItems({text,Icon, active}) {
   return (
     <div className="hoverEffect flex items-center text-gray-700 justify-center xl:justify-start text-lg space-x-3">
     <Icon className="h-7" />
-    <span className={` hidden xl:inline`}>{text} </span>
+    <span className={` ${active && "font-bold"} hidden xl:inline`}>{text} </span>
   </div>
   )
 }
